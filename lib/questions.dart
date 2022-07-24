52800e9d02d3275e77172f5930c1746a5e44335d
Arnaldo-Manjate <leonardomanjate12@gmail.com>
2022-07-24 07:15:53 +0200

    initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,10 @@
+class Question {
+  final String question;
+  final bool answer;
+
+  Question(String this.question, bool this.answer);
+
+  getQuestion() => this.question;
+
+  getAnswer() => this.answer;
+}
